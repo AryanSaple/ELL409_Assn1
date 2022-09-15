343634d163406aebb43fc8609162f936105f7dce
Aryan Saple <saplearyan@gmail.com>
2022-09-15 23:41:27 +0530

    Updated3

--- p1s3.py
+++ p1s3.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -36,21 +35,23 @@ x_test = np.column_stack((x0_test, x_test))
 #Stochastic Gradient Descent Ridge Regression
 w = np.random.rand(7)
 plt.figure()
-for i in range(10000):					#Note that we have 10'000 iterations instead of 1'000 here
+epochs = 1000
+for i in range(N*epochs):					#Note that we have 10'000 iterations instead of 1'000 here
 	sample = (int)(1200*np.random.random())
 	x_s = x[sample, :]
 	t_s = t[sample]
 	y = np.matmul(w.T, x_s)
 	diff = y-t_s
 	alpha = 0.001
-	lamda = 0.1
+	lamda = 0.01
 	regpam = lamda*w
 	regpam[0] = 0
 	grad = diff*x_s + regpam			#Remove w_0 from this equation
 	w = w - alpha*grad
-	loss = np.sum(np.square(np.matmul(x,w) - t))/N + lamda*np.sum(np.square(w))	#confirm this loss function
-	plt.plot (i, loss, 'ro')
-	if (loss < 0.01): break
+	if (i%N == 0):
+		loss = np.sum(np.square(np.matmul(x,w) - t))/N		#confirm this loss function
+		plt.plot (i, loss, 'r.')
+	#if (loss < 0.01): break
 print("Weights using Ridge Regression: ", w)
 print("Final Loss: ", loss)
 
@@ -58,14 +59,15 @@ plt.figure()
 plt.scatter(np.arange(400), t_test)
 y_test = np.matmul(x_test,w)
 plt.scatter(np.arange(400), y_test)
-rmse = np.sqrt(mean_squared_error(t_test, y_test))
+rmse = np.sqrt(np.sum(np.square(y_test - t_test))/400)
 r2 = r2_score(t_test, y_test)
 print(rmse, r2)
 
 #Stochastic Gradient Descent Lasso Regression
 w = np.random.rand(7)
 plt.figure()
-for i in range(10000):					#Note that we have 10'000 iterations instead of 1'000 here
+epochs = 1000
+for i in range(N*epochs):					#Note that we have 10'000 iterations instead of 1'000 here
 	sample = (int)(1200*np.random.random())
 	x_s = x[sample, :]
 	t_s = t[sample]
@@ -77,9 +79,10 @@ for i in range(10000):					#Note that we have 10'000 iterations instead of 1'000
 	regpam[0] = 0
 	grad = diff*x_s + regpam			#Remove w_0 from this equation
 	w = w - alpha*grad
-	loss = np.sum(np.square(np.matmul(x,w) - t))/N		#confirm this loss function
-	plt.plot (i, loss, 'ro')
-	if (loss < 0.01): break
+	if (i%N == 0):
+		loss = np.sum(np.square(np.matmul(x,w) - t))/N		#confirm this loss function
+		plt.plot (i, loss, 'r.')
+	#if (loss < 0.01): break
 print("Weights using Lasso Regression: ", w)
 print("Final Loss: ", loss)
 
@@ -87,7 +90,7 @@ plt.figure()
 plt.scatter(np.arange(400), t_test)
 y_test = np.matmul(x_test,w)
 plt.scatter(np.arange(400), y_test)
-rmse = np.sqrt(mean_squared_error(t_test, y_test))
+rmse = np.sqrt(np.sum(np.square(y_test - t_test))/400)
 r2 = r2_score(t_test, y_test)
 print(rmse, r2)
 
